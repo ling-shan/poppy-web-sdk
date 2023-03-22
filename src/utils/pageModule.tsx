10494b2e01dab6fc14be93455ea516c8370e0a7a
alex.zhang <gzane24@hotmail.com>
2023-03-23 00:53:11 +0800

    init

--- src/utils/pageModule.tsx
+++ src/utils/pageModule.tsx
@@ -5,15 +5,28 @@ import PageShell from 'components/PageShell/PageShell';
 import NotFoundPage from 'components/NotFoundPage/NotFoundPage';
 
 interface CreatePageFactoryOpts {
-  loadPageComponent(pageName?: string): Promise<React.LazyExoticComponent<React.ComponentType<any>>>
+  loadPageComponent(pageName: string): Promise<React.LazyExoticComponent<React.ComponentType<any>>>
 }
 
 export function createPageFactory(opts: CreatePageFactoryOpts): WebModuleFactory {
   return async (context) => {
     let PageComponent: React.ComponentType = NotFoundPage;
+
+    let pageName = context?.params?.page;
+    if (!pageName) {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      pageName = urlSearchParams.get("page") ?? undefined;
+    }
+
+    if (!pageName) {
+      pageName = "notfound";
+    }
+
     try {
-      PageComponent = await opts.loadPageComponent(context?.params?.page);
-    } catch (err) { /* empty */ }
+      PageComponent = await opts.loadPageComponent(pageName);
+    } catch (err) {
+      PageComponent = NotFoundPage;
+    }
 
     const render = ReactDOM.createRoot(context?.container ?? document.getElementById('root') ?? document.body);
     render.render(
