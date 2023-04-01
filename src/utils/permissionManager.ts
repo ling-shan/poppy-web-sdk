4394f0d6ac66976bf6ddb8f3f39c042f4baa0154
alex.zhang <gzane24@hotmail.com>
2023-04-01 20:26:53 +0800

    feat: add the permission manager

---
+++ src/utils/permissionManager.ts
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+type PermissionsData = Record<string, any>;
+
+interface PermissionManager {
+  setPermissions(permissions: PermissionsData): void
+  pushPermissions(permissions: PermissionsData): void
+  popPermissions(): PermissionsData | undefined
+  hasPermission(key: string): boolean;
+}
+
+class PermissionManagerImpl implements PermissionManager {
+  private mergedPermisions: PermissionsData;
+  private permisionsStack: PermissionsData[];
+
+  constructor() {
+    this.mergedPermisions = {};
+    this.permisionsStack = [];
+  }
+
+  private updateMergedPermisions() {
+    const mergedPermisions = {};
+    this.permisionsStack.forEach(permisionsItem => {
+      Object.assign(mergedPermisions, permisionsItem);
+    })
+    this.mergedPermisions = mergedPermisions;
+  }
+
+  setPermissions(permissions: PermissionsData): void {
+    this.permisionsStack = [];
+    this.mergedPermisions = {};
+    Object.assign(this.mergedPermisions, permissions);
+  }
+
+  pushPermissions(permissions: PermissionsData): void {
+    if (!permissions) {
+      permissions = {};
+    }
+
+    this.permisionsStack.push(permissions);
+    this.updateMergedPermisions();
+  }
+
+  popPermissions(): PermissionsData | undefined {
+    if (this.permisionsStack.length <= 0) {
+      return undefined;
+    }
+
+    const permisions = this.permisionsStack.pop();
+    if (!permisions) {
+      return undefined;
+    }
+
+    this.updateMergedPermisions();
+    return permisions;
+  }
+
+  hasPermission(key: string): boolean {
+    return !!this.mergedPermisions[key];
+  }
+}
+
+export const permissionManager = new PermissionManagerImpl();
+
+export default permissionManager;
