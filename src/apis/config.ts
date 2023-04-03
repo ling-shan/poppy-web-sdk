ed88ca3bc5971b61cfda2cd62e740d768063f871
alex.zhang <gzane24@hotmail.com>
2023-04-03 19:04:42 +0800

    fix: dto format

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -1,38 +1,38 @@
-import { Domain } from "../data/Domain";
 import { PagingParams, PagingResult } from "../data/Paging";
+import { Config } from "../data/Config";
 import curl from "../utils/curl"
 
 interface CreateOrUpdateParams {
-  domain: string
+  key: number,
+  value: number,
   remark?: string
-  status?: number
 }
 
 interface ListParams {
-  domain?: string,
-  status?: number
+  key?: string,
+  value?: string
 }
 
 async function list(params: PagingParams<ListParams>) {
-  const response = await curl.get(`/api/poppy/v1/domains`, { params, });
-  return response.data as PagingResult<Domain>
+  const response = await curl.get(`/api/poppy/v1/configs`, { params, });
+  return response.data as PagingResult<Config>
 }
 
 async function create(params: CreateOrUpdateParams) {
-  await curl.post(`/api/poppy/v1/domains`, params);
+  await curl.post(`/api/poppy/v1/configs`, params);
 }
 
 async function update(id: string, params: CreateOrUpdateParams) {
-  await curl.put(`/api/poppy/v1/domains/${id}`, params);
+  await curl.put(`/api/poppy/v1/configs/${id}`, params);
 }
 
 async function get(id: string) {
-  const response = await curl.get(`/api/poppy/v1/domains/${id}`);
-  return response.data as Domain
+  const response = await curl.get(`/api/poppy/v1/configs/${id}`);
+  return response.data as Config
 }
 
 async function del(id: string) {
-  await curl.delete(`/api/poppy/v1/domains/${id}`);
+  await curl.delete(`/api/poppy/v1/configs/${id}`);
 }
 
 export default {
---
+++ src/data/Config.ts
@@ -0,0 +1,12 @@
+
+export interface Config {
+  id: string
+  appId: string
+  status: number
+  createAt: string
+  updateAt: string
+
+  key: string,
+  value: string,
+  remark: string
+}
